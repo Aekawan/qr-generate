5b01cd1eb8227ff7531ea196a74bcd938dd77c0c
Aekkawan Klapsan <aekkawan.kla@thailife.com>
2020-09-02 22:13:39 +0700

    Add /

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const port = 3000
 const pdfController = require('./pdf')
 
 app.get('/pdf', pdfController.pdf)
+app.get('/', (req, res) => {
+  res.json({ message: 'running' })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
