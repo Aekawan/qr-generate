36ce5e81399a3de001830ba7eab1bb16a60e6e21
Aekkawan Klapsan <aekkawan.kla@thailife.com>
2020-10-07 16:11:26 +0700

    Fix url

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -31,7 +31,7 @@ module.exports.pdf = async (req, res, next) => {
   const name = R.pathOr('', ['query', 'name'])(req);
   const type = R.pathOr('', ['query', 'type'])(req);
   const typeTitle = type === 'dine-in' ? 'Dine-in' : 'Take out'
-  const url = type === 'dine-in' ? `http://customer.lacartemenu.com/?res_id=${id}` : `https://lacarte.onelink.me/x6nK?pid=QR_code&c=Restaurant%20Takeout&is_retargeting=true&af_dp=lacartecustomer%3A%2F%2Fmain%2Fexplore%2Frestaurant%2F${id}%2Ftakeout`;
+  const url = type === 'dine-in' ? `http://customer.lacartemenu.com/?res_id=${id}` : `https%3A%2F%2Flacarte.onelink.me%2Fx6nK%3Fpid%3DQR_code%26c%3DRestaurant%2FTakeout%26is_retargeting%3Dtrue%26af_dp%3Dlacartecustomer%3A%2F%2Fmain%2Fexplore%2Frestaurant%2F${id}%2Ftakeout`;
   const qrCodeSize = '157x157';
   const qrCodeUrl = `http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data=${url}&qzone=1&margin=0&size=${qrCodeSize}&ecc=L`;
 
