a2651ca2e6d925b30ff2de05929d977f4073f8aa
Aekkawan Klapsan <aekkawan.kla@thailife.com>
2020-10-15 14:46:34 +0700

    New Takeout template

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -11,7 +11,39 @@ Module._extensions['.png'] = function (module, fn) {
 };
 
 const qrPosition = require('../qr4.json')
-const bg = require('../new_qr_template.png')
+const bg = require('../takeoutposter.png')
+
+const generateQR = (type, image, title) => {
+  if (type === 'dine-in') {
+    const createQr = qrPosition.map(({ x, y }) => ({
+      image: image,
+      width: 157,
+      absolutePosition: { x: x + 0.288, y: y + 0.379 }
+    }))
+    return [
+      ...createQr,
+      {
+        text: title,
+        fontSize: 15,
+        absolutePosition: { x: 35, y: 29.79 },
+        alignment: 'center'
+      }
+    ]
+  }
+  // type === 'take out'
+  return [
+    {
+      image: bg,
+      width: 595,
+      absolutePosition: { x: 0, y: 0 }
+    },
+    {
+      image: image,
+      width: 240,
+      absolutePosition: { x: 55.0709, y: 568.0686 }
+    }
+  ]
+}
 
 module.exports.pdf = async (req, res, next) => {
 
@@ -32,9 +64,9 @@ module.exports.pdf = async (req, res, next) => {
   const type = R.pathOr('', ['query', 'type'])(req);
   const typeTitle = type === 'dine-in' ? 'Dine-in' : 'Take out'
   const url = type === 'dine-in' ? `http://customer.lacartemenu.com/?res_id=${id}` : `https%3A%2F%2Flacarte.onelink.me%2Fx6nK%3Fpid%3DQR_code%26c%3DRestaurant%2FTakeout%26is_retargeting%3Dtrue%26af_dp%3Dlacartecustomer%3A%2F%2Fmain%2Fexplore%2Frestaurant%2F${id}%2Ftakeout`;
-  const qrCodeSize = '157x157';
+  const qrCodeSize = type === 'dine-in' ? '157x157' : '240x240';
   const qrCodeUrl = `http://api.qrserver.com/v1/create-qr-code/?color=000000&bgcolor=FFFFFF&data=${url}&qzone=1&margin=0&size=${qrCodeSize}&ecc=L`;
-
+  const title = `${name} ${code} ${typeTitle}`
   try {
     var result = await axios.get(qrCodeUrl, {
       responseType: 'arraybuffer'
@@ -45,11 +77,7 @@ module.exports.pdf = async (req, res, next) => {
 
   var image = new Buffer.from(result.data, 'base64')
 
-  const createQr = qrPosition.map(({ x, y }) => ({
-    image: image,
-    width: 157,
-    absolutePosition: { x: x + 0.288, y: y + 0.379 }
-  }))
+  const content = generateQR(type, image, title)
 
   var doc = pdfDocument.createPdfKitDocument({
     info: {
@@ -57,12 +85,7 @@ module.exports.pdf = async (req, res, next) => {
       author: 'QR Code',
       subject: 'QR Code',
     },
-    content: [...createQr, {
-      text: `${name} ${code} ${typeTitle}`,
-      fontSize: 15,
-      absolutePosition: { x: 35, y: 29.79 },
-      alignment: 'center'
-    }],
+    content,
   })
 
   let qrCodePdf;
